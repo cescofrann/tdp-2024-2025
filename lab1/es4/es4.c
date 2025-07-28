e0243172f76419bc81e856d1a17922e4fb50f214
Francesco <75941302+cescofrann@users.noreply.github.com>
2025-07-28 12:36:11 +0200

    --

--- lab1/es4/es4.c
+++ lab1/es4/es4.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define P 3.14
+
 int main(){
     char choice, q_route;
     int dim;
@@ -10,21 +12,33 @@ int main(){
 
     switch (choice){
         case 'Q':
-            printf("Calcolo dell'area tramite diagonale (D) o lato (L)? ");
-            scanf(" %c", &q_route);
-            
-            printf("Lunghezza: ");
-            scanf(" %d", &dim);
+            printf("Calcolo dell'area tramite diagonale (D) o lato (L)? Specifica anche la relativa lunghezza senza spazi ");
+            scanf(" %c%d", &q_route, &dim);
 
             if(q_route == 'L'){
                 area = dim*dim;
             }else if (q_route == 'D'){
                 area = dim*dim/2.0;
+            }else{
+                printf("Erroe di scelta");
             }
-            printf("L'area del quadrato è %.2f", area);
+            printf("L'area del quadrato è: %.2f", area);
             break;
 
         case 'C':
+            printf("Calcolo dell'area tramite diametro (D) o raggio (R)? Specifica anche la relativa lunghezza senza spazi ");
+            scanf(" %c%d", &q_route, &dim);
+
+            if (q_route == 'R'){
+                area = P*dim*dim;
+            }else if(q_route == 'D'){
+                dim = dim/2;
+                area = P*dim*dim;
+            }
+            else{
+                printf("Erroe di scelta");
+            }
+            printf("L'area del cerchio è: %.2f", area);
             break;
     }
 }
