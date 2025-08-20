ecca92259c45821c0c6169f86cdcca3ab2c33bf9
Francesco <75941302+cescofrann@users.noreply.github.com>
2025-08-20 12:55:19 +0200

    --

--- lab3/es1/es1.c
+++ lab3/es1/es1.c
@@ -8,7 +8,7 @@ int main(){
     FILE *fp_read, *fp_write;
     char carattere, successivo;
     char *punteggiatura = ".,;:!?'()";
-    int count = 0, nrealchar = 0, flag = 0;
+    int count = 0, nrealchar = 0;
 
     if ((fp_read = fopen("input.txt", "r")) == NULL) {
         printf("Errore di apertura del file");
@@ -22,6 +22,7 @@ int main(){
 
     carattere = fgetc(fp_read);
     while (!feof(fp_read)){
+
         
         if(isdigit(carattere)) {
 
@@ -30,27 +31,30 @@ int main(){
         }else if (strchr(punteggiatura,carattere) != NULL){     
 
             counter(carattere,  &count, 1, &nrealchar, fp_write);
-            flag = 1;
-
-            
             
             if (carattere == '!' || carattere == '?' || carattere == '.'){
                 
                 carattere = fgetc(fp_read);
+
+                if (carattere != ' ' && carattere != '\n' && carattere !=  EOF){
+                    counter(32, &count, 0, &nrealchar, fp_write);
+                }
+
                 while(!isalpha(carattere) && !feof(fp_read)){
                     counter(carattere,  &count, 1, &nrealchar, fp_write);
                     carattere = fgetc(fp_read);
-                    flag = 0;
                 }
 
                 counter(toupper(carattere),  &count, 0, &nrealchar, fp_write);                
+            }else{
+                successivo = fgetc(fp_read);
+                if (successivo != ' ' && successivo != '\n' && successivo !=  EOF){
+                    counter(32, &count, 0, &nrealchar, fp_write);
+                }  
+                carattere = successivo;
+                continue;
             }
-
         }else{
-            if(carattere != ' ' && carattere != '\n' && flag == 1){
-                counter(32, &count, 0, &nrealchar, fp_write);
-            }
-            flag = 0;
             counter(carattere, &count, 1, &nrealchar, fp_write);
         }
         carattere = fgetc(fp_read);
@@ -62,21 +66,21 @@ int main(){
 
 // Ogni volta che viene inserito un carattere si richiama questa funzione che impone un limite a 25 caratteri per riga
 void counter(char c, int *counter, int nrealc, int *countrealc, FILE *fout){
-    *countrealc = *countrealc + nrealc;
-    if(c=='\n' || c =='\0'){
+    if(c=='\n' || c == EOF){
         while(*counter < 25){
             *counter = *counter + 1;
-            printf(" ");
+            fprintf(fout, " ");
         }
         *counter = 0;
-        printf("| c:%d\n", *countrealc);
+        fprintf(fout,"| c:%d\n", *countrealc);
         *countrealc = 0;
     }else{
-        printf("%c",c);
+        *countrealc = *countrealc + nrealc;
+        fprintf(fout, "%c",c);
         *counter = *counter + 1;
         if(*counter == 25){
             *counter = 0;
-            printf("| c:%d\n", *countrealc);
+            fprintf(fout,"| c:%d\n", *countrealc);
             *countrealc = 0;
         }
     }
