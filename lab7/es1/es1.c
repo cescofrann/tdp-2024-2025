3c3239ef7c0737068b43cd9783dcfaec0dd10d24
Francesco <75941302+cescofrann@users.noreply.github.com>
2025-09-09 13:33:30 +0200

    --

--- lab7/es1/es1.c
+++ lab7/es1/es1.c
@@ -6,6 +6,7 @@
 void leggi_mat(FILE *fin, int mat[][MAXC], int nc, int nr);
 void display_mat(int mat[][MAXC], int nc, int nr);
 int core(int mat[][MAXC], int nc, int nr, int scelta);
+int find_width(int mat[][MAXC], int nc, int nr, int coords[], int h);
 
 int main(){
     FILE *fp_read;     
@@ -23,6 +24,8 @@ int main(){
     display_mat(matrice, nc, nr);
 
     core(matrice, nc, nr, 1);
+    core(matrice, nc, nr, 2);
+    core(matrice, nc, nr, 3);
 
     fclose(fp_read);
     return 0;
@@ -46,8 +49,68 @@ void display_mat(int mat[][MAXC], int nc, int nr){
     }
 }
 
+int find_width(int mat[][MAXC], int nc, int nr, int coords[], int h){
+    int l,flag=0,k=0, count;
+
+    l=coords[0];
+    count=1;
+    while(l>0 && mat[l][coords[1]]==1 && !flag){            
+        for(k=coords[1]; k<h && mat[coords[0]][k] == 1; k++);
+
+        if(k==h){
+            l--;
+            count++;
+        }else {
+            flag=1;
+        }
+    }
+
+    l=coords[0];
+    while(l<nc && mat[l][coords[1]] == 1 && !flag){
+        for(k=coords[1]; k<h && mat[coords[0]][k]==1; k++);
+
+        if(k==h){
+            l++;
+            count++;
+        }else{
+            flag=1;
+        }
+    }
+    return count;
+}
+
+int find_height(int mat[][MAXC], int nc, int nr, int coords[], int w){
+    int l,flag=0,k=0, count;
+
+    l=coords[1];
+    count=1;
+    while(l>0 && mat[coords[0]][l]==1 && !flag){            
+        for(k=coords[0]; k<w && mat[k][coords[1]] == 1; k++);
+
+        if(k==w){
+            l--;
+            count++;
+        }else {
+            flag=1;
+        }
+    }
+
+    l=coords[1];
+    while(l<nr && mat[coords[0]][l] == 1 && !flag){
+        for(k=coords[0]; k<w && mat[k][coords[1]]==1; k++);
+
+        if(k==w){
+            l++;
+            count++;
+        }else{
+            flag=1;
+        }
+    }
+    return count;
+}
+
 int core(int mat[][MAXC], int nc, int nr, int scelta){
-    int count=0, max=0, temp_i, larg=0;
+    int count, max=0, temp_i, l=0,h=0;
     int coords[2];
 
     switch (scelta){
@@ -72,16 +135,76 @@ int core(int mat[][MAXC], int nc, int nr, int scelta){
 
                 }
             }
-            for(int k=0; k<max; k++){
-                // conta la larghezza controllando di non sforare si a dx che a sx
-            }
-            printf("Altezza: %d,%d, altezza: %d", coords[0], coords[1], count);
+
+            count=find_width(mat, nc, nr, coords,max);
+            printf("Max altezza: estremo=(%d,%d), altezza=%d, larghezza=%d, area=%d\n", coords[0], coords[1], max, count, count*max);
             break;
-        case 2:
-            break; //Larghezza
-        case 3: //Area
+
+        case 2://Larghezza
+            for(int j=0; j<nr; j++){
+                count = 0; 
+                for(int i=0; i<nc; i++){
+                    if(mat[j][i]==1){
+                        temp_i = i;
+                        while(mat[j][i]==1 && i<nc){
+                            count++;
+                            i++;
+                        }
+                        i--;
+
+                        if(count>max){
+                            max = count;
+                            coords[0]=j;
+                            coords[1]=temp_i;
+                        }
+                    }else count = 0;
+                }
+            }
+
+            count=find_height(mat, nc, nr, coords, max);
+            printf("Max larghezza: estremo=(%d,%d),altezza=%d, larghezza=%d, area=%d\n", coords[0], coords[1], count, max, max*count);
+            break; 
+
+        case 3: // Area
+        int width, height, area, flag;
+            for(int i = 0; i < nr; i++) {
+                for(int j = 0; j < nc; j++) {
+                    if(mat[i][j] == 1) {
+                        l = 0;
+                        h = 1;
+                        while(j + l < nc && mat[i][j + l] == 1) {
+                            l++;
+                        }
+                        
+                        for(int row = i + 1; row < nr; row++) {
+                            flag = 1;
+                            for(int col = j; col < j + l; col++) {
+                                if(mat[row][col] != 1) {
+                                    flag = 0;
+                                    break;
+                                }
+                            }
+                            if(flag) h++;
+                            else break;
+                        }
+                        
+                        area = l * h;
+                        if(area > max) {
+                            max = area;
+                            coords[0] = i;
+                            coords[1] = j;
+                            width = l;
+                            height = h;
+                        }
+                        
+                        j += l - 1; // -1 perché il for fa un j++ di troppo 
+                    }
+                }
+            }
+            printf("Max area: estremo=(%d,%d), altezza=%d, larghezza=%d, area=%d\n", coords[0], coords[1], height, width, max);
             break;
     }
  
     return 0;
 }
+
